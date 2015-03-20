60c914293cf9b453672585fa74cfb5c25571fc28
christian osifre <christianosifre@gmail.com>
2015-03-20 08:36:18 +0100

    first commit

---
+++ app/app.js
@@ -0,0 +1,6 @@
+angular.module('dom', []).controller('MainController', ['$scope', function($scope){
+
+		$scope.description = 'Dom Gilbertson, guitariste, auteur-compositeur et interprète vous invitre dans son univers. Ses chansons sont le fruit d\'un travail approfondit de ce qu\'il aime dans la musique et dans la chanson française. Par sa musique, subtile mélange de musiques andalouses et brésiliennes, il nous fait voyager. Il chante l\'amour, la peine, l\'amtié, la vie dans tous ces états avec beaucoup de talent. Certaines chansons sont pleines d\'humour, d\'autres, empreintes d\'émotions. S\'accompagnant à la guitare il mélange les genres avec délice.';
+		
+  	}]
+);
